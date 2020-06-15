d561be12fbcdb925872be5afa1e4c2f917adc3bc
doannhu9111999 <doannhu9111999@gmail.com>
2020-06-15 17:33:36 +0900

    staff登録用ファイル新規作
    登録フォーム画面
    チェック画面
    完了画面

---
+++ staff/staff_add_done.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title> くるまる農園</title>
+    </head>
+<body>
+
+<?php
+
+try
+{
+
+$staff_name = $_POST['name'];
+$staff_pass = $_POST['pass'];
+
+$staf_name = htmlspeciialchars($taff_name,ENT_QUOTES,'UTF-8');
+$staf_pass = htmlspeciialchars($taff_pass,ENT_QUOTES,'UTF-8');
+
+$dsn = 'mysql:dbname=shop;host=localhost;charset=utf8;
+$user = 'root';
+$password ='';
+$dbh = new PDO($dsn,$user,$password);
+$dbh ->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+$sql='INSERT INTO mst_staff(name,password) VALUES (?,?)';
+$stmt = $dbh->prepare[$sql];
+$data[]=$staff_name;
+$data[]=$staff_pass;
+$stmt->execute[$data];
+
+$dbh = null;
+
+print $staff_name;
+print'さんを追加しました。</br>';
+
+}
+catch[Exception $e]
+{
+    print'ただいま障害により大変ご迷惑をおかけしております。';
+    exit();
+}
+
+?>
+
+
+< a href="staff_list.php"> 戻る </a>
+
+</body>
+ 
+</html>
