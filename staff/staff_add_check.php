199555244dc0606dc1dfdf02eb5009dead65df57
doannhu9111999 <doannhu9111999@gmail.com>
2020-06-29 16:21:56 +0900

    staff登録用ファイル新規作
    - 登録フォーム画面
    - チェック画面
    - 完了画面

--- staff/staff_add_check.php
+++ staff/staff_add_check.php
@@ -47,8 +47,8 @@ else
 {
     $staff_pass=md5($staff_pass);
     print'<form method="post" action="staff_add_done.php">';
-    print'<input type="hiden" name="name" value=".$staff_name.">';
-    print'<input type="hiden" name="pass" value=".$staff_pass.">';
+    print'<input type="hidden" name="name" value="'.$staff_name.'">';
+    print'<input type="hidden" name="pass" value="'.$staff_pass.'">';
     print'<br/>';
     print'<input type="button" onclick="history.back " value="戻る">';
     print'<input type="submit" value="OK">';
--- staff/staff_add_done.php
+++ staff/staff_add_done.php
@@ -14,28 +14,28 @@ try
 $staff_name = $_POST['name'];
 $staff_pass = $_POST['pass'];
 
-$staf_name = htmlspeciialchars($taff_name,ENT_QUOTES,'UTF-8');
-$staf_pass = htmlspeciialchars($taff_pass,ENT_QUOTES,'UTF-8');
+$staff_name = htmlspecialchars($staff_name,ENT_QUOTES,'UTF-8');
+$staff_pass = htmlspecialchars($staff_pass,ENT_QUOTES,'UTF-8');
 
-$dsn = 'mysql:dbname=shop;host=localhost;charset=utf8;
+$dsn = 'mysql:dbname=shop;host=localhost;charset=utf8';
 $user = 'root';
 $password ='';
 $dbh = new PDO($dsn,$user,$password);
-$dbh ->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+$dbh->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
 $sql='INSERT INTO mst_staff(name,password) VALUES (?,?)';
-$stmt = $dbh->prepare[$sql];
+$stmt = $dbh->prepare($sql);
 $data[]=$staff_name;
 $data[]=$staff_pass;
-$stmt->execute[$data];
+$stmt->execute($data);
 
 $dbh = null;
 
 print $staff_name;
-print'さんを追加しました。</br>';
+print'さんを追加しました。<br />';
 
 }
-catch[Exception $e]
+catch(Exception $e)
 {
     print'ただいま障害により大変ご迷惑をおかけしております。';
     exit();
@@ -44,7 +44,7 @@ catch[Exception $e]
 ?>
 
 
-< a href="staff_list.php"> 戻る </a>
+<a href="staff_list.php"> 戻る </a>
 
 </body>
  
